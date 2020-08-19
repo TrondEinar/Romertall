52eaa7af9e7d66c4c2b14897519509ab5caf62c1
TrondEinar <trond-einar98@hotmail.no>
2020-08-19 18:59:43 +0200

    Initial commit

---
+++ src/pgr103_eksamen/Main.java
@@ -0,0 +1,16 @@
+package pgr103_eksamen;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    @Test
+    void shouldConvert1ToI() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private Object toRoman(int i) {
+    }
+}
